30d156ea4de99bdc996db49597ab106a8a04b28f
ywfumav <101041538+ywfumav@users.noreply.github.com>
2023-05-05 08:57:53 +0800

    Create calculator.py

---
+++ week3/calculator.py
@@ -0,0 +1,16 @@
+# Setup
+nameOfTheFruit = str()
+theNumberOfFruitsBought = int()
+priceOfOneFruit = float()
+totalAmountToPay = float()
+
+# Input 
+nameOfTheFruit = input("Enter the name of the fruit bought:")
+theNumberOfFruitsBought = float(input(f"Enter the number of {nameOfTheFruit}:"))
+priceOfOneFruit = float(input(f"Enter the price of a single {nameOfTheFruit}:"))
+
+# Process
+totalAmountToPay = theNumberOfFruitsBought * priceOfOneFruit
+
+# Output 
+print(f"The amount to pay for the {nameOfTheFruit} is ${totalAmountToPay}")
