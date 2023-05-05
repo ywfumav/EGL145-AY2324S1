d6afaf15cba4990131de7f0f368eed8f79ba9885
ywfumav <101041538+ywfumav@users.noreply.github.com>
2023-05-05 08:33:14 +0800

    Update question1.py

--- week3/question1.py
+++ week3/question1.py
@@ -10,4 +10,18 @@ num1 = '3.5'
 num2 = "4"
 
 print("\nQuestion2")
-print(num1 * num2)
+print(num1 + num2)
+
+# Question 3
+num1 = '3.5'
+num2 = 4
+
+print("\nQuestion3")
+print(num1 + num2)
+
+# String Example
+# text1 = "Hello"
+# text2 = " How are you"
+# text3 = "Doing today?"
+# print(text1 + " " +text3 + text2)
+
