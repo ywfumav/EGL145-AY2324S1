6c79f828e5f0645f4f1e0ed44f3ce8bff4df9d00
ywfumav <101041538+ywfumav@users.noreply.github.com>
2023-05-31 09:34:15 +0800

    question 2

--- Lab Test 2 Revision/question2.md
+++ Lab Test 2 Revision/question2.md
@@ -1,5 +1,6 @@
 # Question 2
 
+## Section 1
 Build a random number guessing game that ranges from 1 to 100. User to input an `int` value to guess the random number. The game will only stop when the user guessed the number correctly. 
 
 The game can also provide hints for the user to guess the value correctly.
@@ -13,4 +14,29 @@ Enter a value: 99
 Value is too high, Go Lower!
 Enter a value: 33
 Correct! Well Done! 
+```
+
+## Section 2
+
+### Console Output
+```
+Welcome to the number guessing game
+Enter a value: 10
+Value is too low, Go Higher! 
+Enter a value: 99
+Value is too high, Go Lower!
+Enter a value: 33
+Correct! Well Done! 
+
+Play again (Yes / No)?: Yes
+Enter a value: 20
+Value is too low, Go Higher! 
+Enter a value: 88
+Value is too high, Go Lower!
+Enter a value: 44
+Correct! Well Done! 
+
+Play again (Yes / No)?: No
+Goodbye! 
+
 ```
--- Lab Test 2 Revision/question2.py
+++ Lab Test 2 Revision/question2.py
@@ -1,15 +1,22 @@
 import random
 
 print("Welcome to the number guessing game")
-value = int(input("Enter a value: "))
 ans = random.randint(1, 100)
 print(f"The ans is {ans}")
+value = int(input("Enter a value: "))
+checker = 'yes'
 
-while value != ans:
-    if value < ans:
-        print("Value is too low, Go Higher!")
-    else:
-        print("Value is too high, Go Lower!")
-    value = int(input("Enter a value: "))
+while checker == 'yes':
+    while value != ans:
+        if value < ans:
+            print("Value is too low, Go Higher!")
+        else:
+            print("Value is too high, Go Lower!")
+        value = int(input("Enter a value: "))
 
-print("Correct! Well Done! ")
+    print("Correct! Well Done! ")
+    ans = random.randint(1, 100)
+    checker = input("\nPlay again (Yes / No)?: ")
+    checker = checker.lower()
+
+print("Goodbye!")
