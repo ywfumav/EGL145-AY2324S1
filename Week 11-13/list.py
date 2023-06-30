bc8fc054b14b3afc2c237153cf103d71f9621687
ywfumav <101041538+ywfumav@users.noreply.github.com>
2023-06-30 08:43:38 +0800

    Create list.py

---
+++ Week 11-13/list.py
@@ -0,0 +1,15 @@
+# oned = [100, 200.5, "300", "Four Hundred", 500]
+# print(oned)
+
+# colour = [i for i in range(8)]
+# print(colour)
+# colour = []
+# print(colour)
+# for i in range(8):
+#     colour.append("black")
+#     print(colour)
+
+emp1 = ['John', 'IT', 'M', 5000]
+
+text = emp1[0] + ' from ' + emp1[1]
+print(text)
